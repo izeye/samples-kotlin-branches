103eb791627d27740c540292e6746b194e1e4a29
Johnny Lim <izeye@naver.com>
2020-06-12 15:17:15 +0900

    Add a sample for KDoc

---
+++ src/main/kotlin/kdoc/KDocSample.kt
@@ -0,0 +1,10 @@
+package kdoc
+
+/**
+ * Sample for KDoc.
+ *
+ * See [Documenting Kotlin Code](https://kotlinlang.org/docs/reference/kotlin-doc.html) for more details.
+ *
+ * @author Johnny Lim
+ */
+class KDocSample
